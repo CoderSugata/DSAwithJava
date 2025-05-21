2d79630709da0310b957947408cc62e688e7c21a
S M <hiresugata@gmail.com>
2025-05-21 20:34:30 +0530

    Binary Search Tree

---
+++ 36. BST/BuildBst.java
@@ -0,0 +1,134 @@
+import java.util.*;
+
+public class BuildBst {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node Insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (root.data > val) {
+            // left subtree
+            root.left = Insert(root.left, val);
+        } else {
+            // right subtree
+            root.right = Insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    public static boolean search(Node root, int key) { // O(H)
+        if (root == null) {
+            return false;
+        }
+        if (root.data == key) {
+            return true;
+        }
+        if (root.data > key) {
+            return search(root.left, key);
+        } else {
+            return search(root.right, key);
+        }
+    }
+
+    // delete a Node
+    public static Node delete(Node root, int val) {
+        if (root.data < val) {
+            root.right = delete(root.right, val);
+        } else if (root.data > val) {
+            root.left = delete(root.left, val);
+        } else {
+            // case 1: leaf Node
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            // case 2: single child node
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+            // case 3: both child
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+
+    public static Node findInorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    // Print in range
+    public static void printInRange(Node root, int k1, int k2) {
+        if (root == null) {
+            return;
+        }
+        if (root.data >= k1 && root.data <= k2) {
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data + " ");
+            printInRange(root.right, k1, k2);
+        } else if (root.data < k1) {
+            printInRange(root.left, k1, k2);
+        } else {
+            printInRange(root.right, k1, k2);
+        }
+    }
+
+    // utility function of below
+    public static void printPath(ArrayList<Integer> path) {
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println("N");
+    }
+
+    // Root to leaf Paths
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.data);
+        if (root.left == null && root.right == null) {
+            printPath(path);
+        }
+        printRoot2Leaf(root.left, path);
+        printRoot2Leaf(root.right, path);
+        path.remove(path.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
+        Node root = null;
+
+        for (int i = 0; i < values.length; i++) {
+            root = Insert(root, values[i]);
+        }
+        inOrder(root);
+        System.out.println();
+
+        printRoot2Leaf(root, new ArrayList<>());
+    }
+}
